0271896024eda964d094902f53dada0eed4cc40d
Grégory Copin <gregcop1@gmail.com>
2015-09-22 09:12:31 +0200

    Add config files auto-creation

--- Parameters.php
+++ Parameters.php
@@ -9,19 +9,37 @@ class Parameters extends Script
     {
         $extras = parent::getExtras($event);
         if (!isset($extras['dummyteam-parameters']['parameter-file'])) {
-            throw new \InvalidArgumentException('You have to specify settings parameter-file in  extra.dummyteam-parameters settings.');
+            throw new \InvalidArgumentException('You must set wordpress config file path in  extra.dummyteam-parameters settings.parameter-file');
         }
-        if (!isset($extras['dummyteam-parameters']['parameter-target'])) {
-            $extras['dummyteam-parameters']['parameter-target'] = $extras['webroot-dir'] .'/wp-config.php';
+        if(!file_exists($extras['dummyteam-parameters']['parameter-file'])) {
+            throw new \InvalidArgumentException('File "'.$extras['dummyteam-parameters']['parameter-file'].'" set in extra.dummyteam-parameters.parameter-file doesn\'t exist');
+        }
+        if (!isset($extras['dummyteam-parameters']['destination-folder'])) {
+            $extras['dummyteam-parameters']['destination-folder'] = $extras['webroot-dir'] .'/../';
         }
         return $extras;
     }
 
     public static function build(Event $event)
     {
-        $config = self::getExtras($event);
+        // manage package configuration
+        $globalConfig = self::getExtras($event);
+        $config = $globalConfig['dummyteam-parameters'];
 
-        print_r($config);
+        // prepare output
+        $io = $event->getIO();
+
+        // copy source file to destination
+        $distFile = $config['destination-folder'].'wp-config.php.dist';
+        if(!file_exists($distFile)) {
+            $io->write(sprintf('<info>Create "%s" file</info>', $distFile));
+            copy($config['parameter-file'], $distFile);
+        }
+        $targetFile = $config['destination-folder'].'wp-config.php';
+        if(!file_exists($targetFile)) {
+            $io->write(sprintf('<info>Create "%s" file</info>', $targetFile));
+            copy($config['parameter-file'], $targetFile);
+        }
     }
 
 }
