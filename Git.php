bd0781888828c254bb15eed4b7692626174fecf4
Grégory Copin <gregcop1@gmail.com>
2015-09-23 13:32:31 +0200

    fix a typo in variable ask

--- Git.php
+++ Git.php
@@ -11,18 +11,19 @@ class Git
         $io = $event->getIO();
 
         $ask = 'Do you wan\'t to create a Git Repository? ';
-        $hasToCreateAGitRepository = $io->askConfirmation(sprintf('<question>%s</question>: ', ask), true);
+        $hasToCreateAGitRepository = $io->askConfirmation(sprintf('<question>%s</question>: ', $ask), true);
 
         if($hasToCreateAGitRepository) {
             // repository initialization
             exec('git init');
-            $io->write('<info>Git repository initialization</info>');
+            $io->write(PHP_EOL.'<info>Git repository initialization</info>'.PHP_EOL);
 
             // remote initialization
             $ask = 'Git repository remote URL';
             $comment = 'let it blank if don\'t have one yet';
-            $repositoryUrl = $io->ask(sprintf('<question>%s</question> (<comment>%s</comment>): ', ask, $comment), '');
+            $repositoryUrl = $io->ask(sprintf('<question>%s</question> (<comment>%s</comment>): ', $ask, $comment), '');
             if($repositoryUrl != '') {
+                $io->write(PHP_EOL.sprintf('<info>Set repository origin as: %s</info>', $repositoryUrl).PHP_EOL);
                 exec(sprintf('git remote add origin %s', $repositoryUrl));
             }
         }
